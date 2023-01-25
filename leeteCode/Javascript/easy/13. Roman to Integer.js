1ab78ed318b70ad727442014ea28af7036569900
AhmedDadousha <Bioshock44matrix44@gmail.com>
2023-01-25 19:52:10 +0200

    Roman to integer:v 0.0.2

--- leeteCode/Javascript/easy/13. Roman to Integer.js
+++ leeteCode/Javascript/easy/13. Roman to Integer.js
@@ -8,28 +8,35 @@ let romanToInt = (s) => {
 	const romanNumbers = [1, 5, 10, 50, 100, 500, 1000];
 
 	for (let i = 0; i < s.length; i++) {
-		if (s[i] + s[i + 1] === 'IV') {
-			output += 4;
-			i += 1;
-		} else if (s[i] + s[i + 1] === 'IX') {
-			output += 9;
-			i += 2;
-		} else if (s[i] + s[i + 1] === 'XL') {
-			output += 40;
-			i++;
-		} else if (s[i] + s[i + 1] === 'XC') {
-			output += 90;
-			i += 1;
-		} else if (s[i] + s[i + 1] === 'CD') {
-			output += 400;
-			i += 1;
-		} else if (s[i] + s[i + 1] === 'CM') {
-			output += 900;
-			i += 1;
-		} else {
-			for (let j = 0; j < romanNumbersRepresentation.length; j++) {
-				if (s[i] === romanNumbersRepresentation[j]) output += romanNumbers[j];
-			}
+		switch (s[i] + s[i + 1]) {
+			case 'IV':
+				output += 4;
+				i += 1;
+				break;
+			case 'IX':
+				output += 9;
+				i += 1;
+				break;
+			case 'XL':
+				output += 40;
+				i += 1;
+				break;
+			case 'XC':
+				output += 90;
+				i += 1;
+				break;
+			case 'CD':
+				output += 400;
+				i += 1;
+				break;
+			case 'CM':
+				output += 900;
+				i += 1;
+				break;
+			default:
+				for (let j = 0; j < romanNumbersRepresentation.length; j++) {
+					if (s[i] === romanNumbersRepresentation[j]) output += romanNumbers[j];
+				}
 		}
 	}
 
